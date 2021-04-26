47130bc79584ddc9d01a855b9a9bb0a0984bf633
ribeshbasnet <ribeshbasnet19.rb@gmail.com>
2021-04-26 17:18:13 +0545

    Refactor code

--- src/main/java/ com/github/ribeshbasnet/kafka/project1/ConsumerDemo.java
+++ src/main/java/ com/github/ribeshbasnet/kafka/project1/ConsumerDemo.java
@@ -4,7 +4,6 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.protocol.types.Field;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
